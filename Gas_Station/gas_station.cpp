217f3d18adc3a05419ba29207f82e24041e8477b
Summon Yang <SummonYang1991@gmail.com>
2015-10-06 15:24:30 +0800

    gas station

---
+++ Gas_Station/gas_station.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    /**
+     * @param gas: a vector of integers
+     * @param cost: a vector of integers
+     * @return: an integer 
+     */
+    int canCompleteCircuit(vector<int> &gas, vector<int> &cost) {
+        int i, index = -1;
+        int sum = 0, total = 0;
+        for (i = 0; i < gas.size(); ++i) {
+            sum += gas[i] - cost[i];
+            total += gas[i] - cost[i];
+            if (sum < 0) {
+                index = i;
+                sum = 0;
+            }
+        }
+        return total >= 0 ? index + 1 : -1;
+    }
+};
