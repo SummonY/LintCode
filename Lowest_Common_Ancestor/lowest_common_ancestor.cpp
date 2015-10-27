1671c211b6ffaa8325ec9abbc7ba465e852cc579
Summon Yang <SummonYang1991@gmail.com>
2015-10-27 14:50:36 +0800

    Lowest Common Ancestor

---
+++ Lowest_Common_Ancestor/lowest_common_ancestor.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition of TreeNode:
+ * class TreeNode {
+ * public:
+ *     int val;
+ *     TreeNode *left, *right;
+ *     TreeNode(int val) {
+ *         this->val = val;
+ *         this->left = this->right = NULL;
+ *     }
+ * }
+ */
+class Solution {
+public:
+    /**
+     * @param root: The root of the binary search tree.
+     * @param A and B: two nodes in a Binary.
+     * @return: Return the least common ancestor(LCA) of the two nodes.
+     */
+    TreeNode *lowestCommonAncestor(TreeNode *root, TreeNode *A, TreeNode *B) {
+        if (root == NULL || root == A || root == B) {
+            return root;
+        }
+        TreeNode *left = lowestCommonAncestor(root->left, A, B);
+        TreeNode *right = lowestCommonAncestor(root->right, A, B);
+        if (left != NULL && right != NULL) {
+            return root;
+        }
+        if (left != NULL) {
+            return left;
+        }
+        if (right != NULL) {
+            return right;
+        }
+        return NULL;
+    }
+};
